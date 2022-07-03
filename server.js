c0576b99d0267d4e188384f412dc0d84e519d623
Jan Torge Schneider <jantorge.schneider@haw-hamburg.de>
2022-07-03 04:29:27 +0200

    Setup Server

---
+++ server.js
@@ -0,0 +1,20 @@
+const WebSocket = require('ws')
+
+const wss = new WebSocket.Server({ port: 4200 },()=>
+{
+    console.log('server started')
+})
+
+wss.on('connection', function connection(ws) 
+{
+   ws.on('message', (data) => 
+   {
+      console.log('data received: ' + data.toString())
+      ws.send(data.toString());
+   })
+})
+
+wss.on('listening',()=>
+{
+   console.log('listening on 4200')
+})
